7cb3001d3e9ce16455e7eede90ec86d22bdfc290
Iris Jeuffrard <114398946+ijeuffrard@users.noreply.github.com>
2023-05-30 17:45:13 +0200

    add geoserver layer

--- serveur.js
+++ serveur.js
@@ -34,6 +34,8 @@ app.use('/vendor', express.static(`${__dirname}/resources/vendor`));
 app.use('/css', express.static(`${__dirname}/resources/css`));
 app.use('/js', express.static(`${__dirname}/resources/js`));
 app.use('/chart.js', express.static(`${__dirname}/node_modules/chart.js`));
+app.use('/leaflet', express.static(`${__dirname}/node_modules/leaflet`));
+app.use('/leaflet-geoserver-request', express.static(`${__dirname}/node_modules/leaflet-geoserver-request`));
 app.use('/images', express.static(`${__dirname}/resources/images`));
 
 // use res.render to load up an ejs view file
